ff6de2050ffd0508177239d98d30fbe6a4dbf9f5
Alesia Kolbik <moyaalesia@mail.ru>
2017-10-11 23:32:56 +0300

    change main

--- src/com/testpackage/Main.java
+++ src/com/testpackage/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         int x = (int) (Math.random() * 21 - 10);
+
         System.out.println(x);
+
         if (x > 0) {
             if (x % 2 == 0) {
                 System.out.println("Число четное!");
